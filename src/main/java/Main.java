000b5973eed651302d248f6429fa32b965f8905c
Julieta <julietataseva@gmail.com>
2022-01-01 23:56:36 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import dao.CompanyDAO;
+import entity.Company;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String args[]) {
+        
+    }
+}
