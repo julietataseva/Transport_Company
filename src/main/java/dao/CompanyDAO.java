244ebd02e71a6793adfbb0fdc6fa7e5f59ba03a5
Julieta <julietataseva@gmail.com>
2022-01-02 00:22:31 +0200

    added saveCompany and saveOrUpdateCompany methods

--- src/main/java/dao/CompanyDAO.java
+++ src/main/java/dao/CompanyDAO.java
@@ -1,4 +1,23 @@
 package dao;
 
+import entity.Company;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class CompanyDAO {
+    public static void saveCompany(entity.Company company) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(company);
+            transaction.commit();
+        }
+    }
+
+    public static void saveOrUpdateCompany(Company company) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(company);
+            transaction.commit();
+        }
+    }
 }
