d762dd951073c97d46034cc332c14575918d91a1
Julieta <julietataseva@gmail.com>
2022-01-02 23:12:11 +0200

    added CRUD methods for client

--- src/main/java/dao/ClientDAO.java
+++ src/main/java/dao/ClientDAO.java
@@ -1,4 +1,39 @@
 package dao;
 
+import entity.Client;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class ClientDAO {
+    public static void saveClient(Client client) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(client);
+            transaction.commit();
+        }
+    }
+
+    public static List<Client> readClients() {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT c FROM Client c", entity.Client.class).getResultList();
+        }
+    }
+
+    public static void saveOrUpdateClient(Client client) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(client);
+            transaction.commit();
+        }
+    }
+
+    public static void deleteClient(Client client) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(client);
+            transaction.commit();
+        }
+    }
 }
