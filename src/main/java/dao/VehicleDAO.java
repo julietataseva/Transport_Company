d518be0b9f95a579189275e87880b69d2e2221c1
Julieta <julietataseva@gmail.com>
2022-01-02 23:38:56 +0200

    added CRUD methods for vehicle

--- src/main/java/dao/VehicleDAO.java
+++ src/main/java/dao/VehicleDAO.java
@@ -1,4 +1,39 @@
 package dao;
 
+import entity.Vehicle;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class VehicleDAO {
+    public static void saveVehicle(Vehicle vehicle) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(vehicle);
+            transaction.commit();
+        }
+    }
+
+    public static List<Vehicle> readVehicles() {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT v FROM Vehicle v", entity.Vehicle.class).getResultList();
+        }
+    }
+
+    public static void saveOrUpdateVehicle(Vehicle vehicle) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(vehicle);
+            transaction.commit();
+        }
+    }
+
+    public static void deleteVehicle(Vehicle vehicle) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(vehicle);
+            transaction.commit();
+        }
+    }
 }
