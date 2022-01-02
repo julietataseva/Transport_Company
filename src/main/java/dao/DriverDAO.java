09d8500014111d880c7569bef2165f72d5ca47e7
Julieta <julietataseva@gmail.com>
2022-01-02 23:15:32 +0200

    added CRUD methods for driver

--- src/main/java/dao/DriverDAO.java
+++ src/main/java/dao/DriverDAO.java
@@ -1,4 +1,39 @@
 package dao;
 
+import entity.Driver;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class DriverDAO {
+    public static void saveDriver(Driver driver) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(driver);
+            transaction.commit();
+        }
+    }
+
+    public static List<Driver> readDrivers() {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT d FROM Driver d", entity.Driver.class).getResultList();
+        }
+    }
+
+    public static void saveOrUpdateDriver(Driver driver) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(driver);
+            transaction.commit();
+        }
+    }
+
+    public static void deleteDriver(Driver driver) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(driver);
+            transaction.commit();
+        }
+    }
 }
