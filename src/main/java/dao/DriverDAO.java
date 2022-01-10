56ac27d9a230ddb40ba0c45b4e9b26a5d95d0648
Julieta <julietataseva@gmail.com>
2022-01-10 19:14:27 +0200

    added methods filterByQualificationType, filterBySalary, sortByQualificationType, sortBySalary

--- src/main/java/dao/DriverDAO.java
+++ src/main/java/dao/DriverDAO.java
@@ -3,15 +3,22 @@ package dao;
 import entity.Driver;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class DriverDAO {
     public static void saveDriver(Driver driver) {
-        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.save(driver);
-            transaction.commit();
+        if (driver != null) {
+            try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+                Transaction transaction = session.beginTransaction();
+                session.save(driver);
+                transaction.commit();
+            }
         }
     }
 
@@ -22,10 +29,12 @@ public class DriverDAO {
     }
 
     public static void saveOrUpdateDriver(Driver driver) {
-        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.saveOrUpdate(driver);
-            transaction.commit();
+        if (driver != null) {
+            try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+                Transaction transaction = session.beginTransaction();
+                session.saveOrUpdate(driver);
+                transaction.commit();
+            }
         }
     }
 
@@ -47,5 +56,57 @@ public class DriverDAO {
             transaction.commit();
         }
         return driver;
+    }
+
+    public static List<Driver> filterByQualificationType(Driver.QualificationType qualificationType) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Driver> cr = cb.createQuery(Driver.class);
+            Root<Driver> root = cr.from(Driver.class);
+            cr.select(root).where(cb.equal(root.get("qualificationType"), qualificationType));
+
+            Query<Driver> query = session.createQuery(cr);
+            List<Driver> drivers = query.getResultList();
+            return drivers;
+        }
+    }
+
+    public static List<Driver> filterBySalary(BigDecimal salary) {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Driver> cr = cb.createQuery(Driver.class);
+            Root<Driver> root = cr.from(Driver.class);
+            cr.select(root).where(cb.equal(root.get("salary"), salary));
+
+            Query<Driver> query = session.createQuery(cr);
+            List<Driver> drivers = query.getResultList();
+            return drivers;
+        }
+    }
+
+    public static List<Driver> sortByQualificationType() {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Driver> cr = cb.createQuery(Driver.class);
+            Root<Driver> root = cr.from(Driver.class);
+            cr.select(root).orderBy(cb.asc(root.get("qualificationType")));
+
+            Query<Driver> query = session.createQuery(cr);
+            List<Driver> drivers = query.getResultList();
+            return drivers;
+        }
+    }
+
+    public static List<Driver> sortBySalary() {
+        try (Session session = configuration.SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Driver> cr = cb.createQuery(Driver.class);
+            Root<Driver> root = cr.from(Driver.class);
+            cr.select(root).orderBy(cb.asc(root.get("salary")));
+
+            Query<Driver> query = session.createQuery(cr);
+            List<Driver> drivers = query.getResultList();
+            return drivers;
+        }
     }
 }
